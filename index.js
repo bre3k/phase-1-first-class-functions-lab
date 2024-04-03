741942068780b17692aaee63046a8b9d44fabccf
Brenda <brendakwambokam@gmail.com>
2024-04-03 20:00:13 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function receivesAFunction(callback) {
+  callback()
+}
+
+function returnsANamedFunction() {
+  return function named() {
+    console.log(The girl is Arya.)
+  }
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log('The girl has no name.')
+  }
+}
